47ca13e702afac6be7217ab6910991bf2e06b45a
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2025-07-09 15:38:19 +0200

    nav werkend krijgen

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12">
         <div class="relative flex justify-between items-center h-20">
 
-            <div class="absolute z-10 pointer-events-none left-1/2 transform -translate-x-1/2" style="width: max-content;">
+            <div class="absolute left-1/2 transform -translate-x-1/2" style="width: max-content;">
                 <a href="{{ route('home') }}" class="text-2xl font-semibold font-serif text-gray-800 tracking-tight pointer-events-auto">
                     aMayzing
                 </a>
