625189d7729d47a4e64d3fc4c8127fcfb48ec5d8
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2025-07-09 16:59:28 +0200

    test

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -141,7 +141,7 @@
                     @if ($product->image)
                         <img src="{{ asset('storage/' . $product->image) }}"
                               alt="{{ $product->name }}"
-                              class="rounded-xl mb-4 max-h-32 w-full object-contain bg-white">
+                              class="rounded-xl mb-4 max-h-36 w-full object-contain bg-white">
                     @else
                         <img src="{{ asset('images/placeholder.jpg') }}"
                              alt="Geen afbeelding beschikbaar"
