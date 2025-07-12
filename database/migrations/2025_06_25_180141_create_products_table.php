dd425ca9d5273a700d6e7db941ca4804e80b35ee
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2025-07-12 18:50:44 +0200

    test

--- database/migrations/2025_06_25_180141_create_products_table.php
+++ database/migrations/2025_06_25_180141_create_products_table.php
@@ -11,17 +11,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
-            $table->integer('pickup_stock')->default(0);
-            $table->integer('delivery_stock')->default(0);
-            $table->string('image')->nullable();
-            $table->boolean('is_active')->default(true);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('products')) {
+            Schema::create('products', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->text('description')->nullable();
+                $table->decimal('price', 8, 2);
+                $table->integer('pickup_stock')->default(0);
+                $table->integer('delivery_stock')->default(0);
+                $table->string('image')->nullable();
+                $table->boolean('is_active')->default(true);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
