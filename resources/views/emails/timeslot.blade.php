cc135dc9e0872ca9b1b31c9636dc7b2709878b81
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2025-10-05 12:07:53 +0200

    mail timeslot aangepast

--- resources/views/emails/timeslot.blade.php
+++ resources/views/emails/timeslot.blade.php
@@ -11,6 +11,13 @@
 
 <p>We kijken ernaar uit om uw bestelling te bezorgen!</p>
 
+<p>
+    Voor vragen of opmerkingen kunt u een bericht sturen naar
+    <strong>
+        <a href="tel:+31644042554" style="color:#000; text-decoration:none;">+31 6 44 04 25 54</a>
+    </strong>.
+</p>
+
 <p>Met vriendelijke groet,<br>
     Het aMayzing team</p>
 </body>
