8f42d4c72c1708459e924edfa8e23423616a7618
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2026-02-24 23:57:42 +0100

    dinsdag niet meer in pickup rooster

--- config/pickup.php
+++ config/pickup.php
@@ -9,14 +9,14 @@
                 'addition' => 'A',
                 'postcode' => '1521 NA',
                 'city' => 'Wormerveer',
-                'days' => 'Dinsdag t/m Zaterdag: 10:00 – 15:00',
+                'days' => 'Woensdag t/m Zaterdag: 10:00 – 15:00',
                 'hours' => [
                     'maandag' => ['open' => '00:00', 'close' => '00:00'], // gesloten
-                    'dinsdag' => ['open' => '11:00', 'close' => '15:00'],
-                    'woensdag' => ['open' => '11:00', 'close' => '15:00'],
-                    'donderdag' => ['open' => '11:00', 'close' => '15:00'],
-                    'vrijdag' => ['open' => '11:00', 'close' => '15:00'],
-                    'zaterdag' => ['open' => '11:00', 'close' => '15:00'],
+                    'dinsdag' => ['open' => '00:00', 'close' => '00:00'], // gesloten
+                    'woensdag' => ['open' => '10:00', 'close' => '15:00'],
+                    'donderdag' => ['open' => '10:00', 'close' => '15:00'],
+                    'vrijdag' => ['open' => '10:00', 'close' => '15:00'],
+                    'zaterdag' => ['open' => '10:00', 'close' => '15:00'],
                     'zondag' => ['open' => '00:00', 'close' => '00:00'], // gesloten
                 ],
             ],
